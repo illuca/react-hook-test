413274abd10efb31536baa53fc8cf7524a8aa4e8
Yuan Long <longyuan@magiccreative.tech>
2022-04-11 23:33:33 +0800

    函数组件使用hook后，也能进行更新状态值

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{Component} from "react";
+
+class Count extends Component {
+    state = {
+        count: 0
+    }
+    add = () => {
+        this.setState((state) => {
+            return {count: state.count + 1}
+        })
+    }
+
+    render() {
+        return <div>
+            <p>Count: {this.state.count}</p>
+            <button onClick={this.add}>点我+1</button>
+        </div>;
+    }
+}
+
+function FunctionCount() {
+    const [count, setCount] = React.useState(0)
+    function add() {
+        console.log(count)
+        setCount(count+1)
+    }
+    return <div>
+        <p>Count: {count}</p>
+        <button onClick={add}>点我+1</button>
+    </div>
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <FunctionCount/>
+        </div>
+    );
 }
 
 export default App;
