43296278fad8f0bd5331344e19584bb066c794d7
Yuan Long <longyuan@magiccreative.tech>
2022-04-12 23:32:35 +0800

    useEffect_Hook

--- src/App.js
+++ src/App.js
@@ -22,15 +22,25 @@ class Count extends Component {
 
 function FunctionCount() {
     const [count, setCount] = React.useState(0)
+    const [personName, setPersonName] = React.useState('jack')
+
+    React.useEffect(()=>{
+        console.log('@')
+    }, [count])
 
     function add() {
         console.log(count)
         setCount(count + 1)
     }
+    function changePersonName() {
+        setPersonName('fuck')
+    }
 
     return <div>
         <p>Count: {count}</p>
+        <p>PersonName: {personName}</p>
         <button onClick={add}>点我+1</button>
+        <button onClick={changePersonName}>改名字</button>
     </div>
 }
 
