c4914457a73a56148d3cd6714cb147a0d40366d8
Yuan Long <longyuan@magiccreative.tech>
2022-04-12 23:07:49 +0800

    函数组件使用hook后，也能进行更新状态值

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React,{Component} from "react";
+import React, {Component} from "react";
 
 class Count extends Component {
     state = {
@@ -22,10 +22,12 @@ class Count extends Component {
 
 function FunctionCount() {
     const [count, setCount] = React.useState(0)
+
     function add() {
         console.log(count)
-        setCount(count+1)
+        setCount(count + 1)
     }
+
     return <div>
         <p>Count: {count}</p>
         <button onClick={add}>点我+1</button>
